67e4e0b98229b1cda095d5d8c46b828a2a1f38e3
yuwantao <yuwantao1109@gmail.com>
2012-11-23 16:51:51 +0800

    add UniversityDao class for university persistence.

---
+++ persistence/src/main/java/com/qingdao/persistence/dao/UniversityDao.java
@@ -0,0 +1,12 @@
+package com.qingdao.persistence.dao;
+
+/**
+ * @author <a href="mailto:wantao.yu@renren-inc.com">wantao.yu</a>
+ *         Created at 12-11-23 下午4:18
+ */
+public class UniversityDao {
+
+    public static void main(String[] args) {
+        System.out.println("Yes, this is the feeling.");
+    }
+}
